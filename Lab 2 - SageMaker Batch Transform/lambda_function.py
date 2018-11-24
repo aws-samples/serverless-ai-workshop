138d67aaa2bbf3262d91d8651517d9fbe5e924bf
Skrinak <skrinak@186590d28a3f.ant.amazon.com>
2018-11-24 13:44:45 -0800

    Fixed typos, added command line test to batch transform

--- Lab 2 - SageMaker Batch Transform/lambda_function.py
+++ Lab 2 - SageMaker Batch Transform/lambda_function.py
@@ -1,10 +1,8 @@
-import warnings
 import boto3
-import json
 import sagemaker
+import json
+from sagemaker.session import Session
 
-#Warning is the base class so it should catch all types of warnings
-warnings.filterwarnings(action='ignore', category=Warning) 
 s3 = boto3.resource('s3')
 
 # make Prediction on the trained model
@@ -12,34 +10,50 @@ def lambda_handler(event, context):
 
 	# Bucket_name and model name are passed on from event
 	data = json.loads(json.dumps(event))
-	BUCKET_NAME = data['bucket_name']
-	MODEL_NAME = data['model_name']
+	BUCKET = data['bucket']
+	MODEL = data['model']
 	
 	# Initialize onstants variables
-	VALIDATION_FILE = 'validation_data.csv'
-	VALIDATION_KEY = 'ServerlessAIWorkshop/data/' + VALIDATION_FILE
-	PREDICTION_FOLDER = 'ServerlessAIWorkshop/prediction/'
-	PREDICTION_KEY = PREDICTION_FOLDER + VALIDATION_FILE + ".out"
-	INPUT_URL = 's3://{}/{}'.format(BUCKET_NAME, VALIDATION_KEY)
-	OUTPUT_URL = 's3://{}/{}'.format(BUCKET_NAME, PREDICTION_FOLDER)
-	
+	sagemaker_session = sagemaker.Session()
+	region = sagemaker_session.boto_session.region_name
+	INPUT_BUCKET = 'sagemaker-sample-data-{}'.format(region)
+	DATADIR = 'batch-transform/mnist-1000-samples'
+	OUTPUT = 'ServerlessAIWorkshop/BatchTransform/output'
+
+	input_key = 'kmeans_batch_example/input/valid-data.csv'
+	input_location = 's3://{}/{}'.format(INPUT_BUCKET, DATADIR)
+	output_location = 's3://{}/{}'.format(BUCKET, OUTPUT)
+
 	# Initialize the transformer object
 	transformer =sagemaker.transformer.Transformer(
-		base_transform_job_name='Batch-Transform',
-		model_name=MODEL_NAME,
-		instance_count=1,
-		instance_type='ml.c4.xlarge',
-		output_path=OUTPUT_URL
+		base_transform_job_name='Serverless-Workshop',
+		model_name=MODEL,
+		instance_count=4,
+		instance_type='ml.c5.18xlarge',
+		output_path=output_location
 		)
 	
 	# To start a transform job:
-	transformer.transform(INPUT_URL, content_type='text/csv', split_type='Line')
+	transformer.transform(input_location, content_type='text/csv', split_type='Line')
 
 	# Then wait until transform job is completed
 	transformer.wait()
-	
-	
+
+	s3 = boto3.resource('s3')
+	predictions = []
+	results = []
+	for i in range(10):
+		file_key = '{}/data-{}.csv.out'.format(OUTPUT, i)
+
+		output_obj = s3.Object(BUCKET, file_key)
+		output = output_obj.get()["Body"].read().decode('utf-8')
+
+		predictions.extend(json.loads(output)['outputs']['classes']['int64Val'])
+
+	originals='7, 2, 1, 0, 4, 1, 4, 9, 5, 9'
+	predictions=', '.join(predictions)
+
 	return {
         "statusCode": 200,
-        "body": json.dumps('Hello from Lambda!')
+        "body": json.dumps({'originals': originals, 'predictions': predictions})
     }
