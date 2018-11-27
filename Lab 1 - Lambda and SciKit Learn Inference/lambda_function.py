33182df88542846de8176ff866d5ab5eb5f408ac
Kris Skrinak <skrinak@gmail.com>
2018-11-27 13:21:30 -0800

    Updated single tweet results

--- Lab 1 - Lambda and SciKit Learn Inference/lambda_function.py
+++ Lab 1 - Lambda and SciKit Learn Inference/lambda_function.py
@@ -1,55 +1,43 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.externals import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import warnings
-import boto3
-import random
 import json
+import boto3
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.externals import joblib
 
-#Warning is the base class so it should catch all types of warnings
-warnings.filterwarnings(action='ignore', category=Warning) 
 s3 = boto3.resource('s3')
 
 # make Prediction on the trained model
-def lambda_handler(event, context):
+def lambda_handler (event, context):
 
 	# Bucket_name is passed on from event
 	data = json.loads(json.dumps(event))
-	BUCKET_NAME = data['bucket_name']
-	#TEST_TWEET_NEG = data['test_tweet_neg']
-	#TEST_TWEET_POS = data['test_tweet_pos']
 	
-	# Initialize onstants variables
-	PICKLE_FILE_NAME = '/tmp/sentiment_analysis_tweet.pkl'
-	PICKLE_FILE_KEY = 'ServerlessAIWorkshop/SentimentAnalysis/sentiment_analysis_tweet.pkl'
-	X_TEST = '/tmp/X_test.data'
-	y_TEST = '/tmp/y_test.data'
-	X_TEST_FILE_KEY = 'ServerlessAIWorkshop/SentimentAnalysis/X_test.data'
-	y_TEST_FILE_KEY = 'ServerlessAIWorkshop/SentimentAnalysis/y_test.data'
-
+	# Initialize constants 
+	BUCKET_NAME = data['bucket_name']
+	DATADIR='ServerlessAIWorkshop/SentimentAnalysis'
+	MODEL_FILE = 'sentiment_analysis_model.pkl'
+	VOCAB_FILE = 'tweet_vocabulary.pkl'
+	MODEL_FILE_KEY = DATADIR + '/' + MODEL_FILE
+	VOCAB_FILE_KEY = DATADIR + '/' + VOCAB_FILE
+	LOCAL_MODEL_FILE = '/tmp/' +  MODEL_FILE
+	LOCAL_VOCAB_FILE = '/tmp/' +  VOCAB_FILE
+	TEST_TWEET = data['test_tweet']
 	
 	# get trained model/pkl file from S3 bucket
-	s3.Object(BUCKET_NAME, PICKLE_FILE_KEY).download_file(PICKLE_FILE_NAME)
-	# get dataset files from s3 bucket
-	s3.Object(BUCKET_NAME, X_TEST_FILE_KEY).download_file(X_TEST)
-	s3.Object(BUCKET_NAME, y_TEST_FILE_KEY).download_file(y_TEST)
-	
+	s3.Object(BUCKET_NAME, MODEL_FILE_KEY).download_file(LOCAL_MODEL_FILE)
+	s3.Object(BUCKET_NAME, VOCAB_FILE_KEY).download_file(LOCAL_VOCAB_FILE)
+
 	# loading the trained model as well as test data and label
-	model = joblib.load(PICKLE_FILE_NAME) 
-	X_test = joblib.load(X_TEST)
-	y_test = joblib.load(y_TEST)
-	
-	# predict using the test dataset
-	y_pred = model.predict(X_test)
-	
-	# evaluate predictions
-	accuracy = accuracy_score(y_test, y_pred)
-	
-	print("Prediction for the first test tweet is " + model.predict([X_test[0]])[0])
+	model = joblib.load(LOCAL_MODEL_FILE) 
+	vocabulary_to_load =joblib.load(LOCAL_VOCAB_FILE)
+	loaded_vectorizer = CountVectorizer(vocabulary=vocabulary_to_load)
+
+	transformed_tweets = loaded_vectorizer.transform([TEST_TWEET])
+	transformed_tweets = transformed_tweets.toarray()
+
+	sentiment = model.predict(transformed_tweets)
+	results = {'sentiment': sentiment[0] }
 	
 	return {
         "statusCode": 200,
-        "body": json.dumps("Accuracy from the validation dataset: %.2f%%" % (accuracy * 100.0))
+        "body": json.dumps(results)
     }
